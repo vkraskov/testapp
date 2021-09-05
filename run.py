ebaaa6b83f342f7732cfb6c150a158aa6dfd13ae
Vasiliy <vkraskov@gmail.com>
2021-09-05 16:20:27 +0300

    Create run.py

---
+++ run.py
@@ -0,0 +1,13 @@
+import uvicorn
+from os import environ
+
+if __name__ == "__main__":
+
+    config = { 'app_host': '0.0.0.0', 'app_port': 8080 }
+
+    if 'APP_API_HOST' in environ:
+        config['app_host'] = environ['APP_API_HOST']
+    if 'APP_API_PORT' in environ:
+        config['app_port'] = environ['APP_API_PORT']
+
+    uvicorn.run("main:app", host=config['app_host'], port=config['app_port'], reload=True)
